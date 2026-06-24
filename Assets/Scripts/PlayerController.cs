5b6055c788127d47991044366ad19330a6e7f7ec
SON9797 <zxcvcxz95@naver.com>
2026-06-24 15:43:22 +0900

    Feat : 플레이어 이동, 점프

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     [Header("플레이어 설정")]
     [SerializeField] private float _playerSpeed = 3f;
-    [SerializeField] private float _jumpForce = 4f;
+    [SerializeField] private float _walkJumpForce = 6f;
+    [SerializeField] private float _runJumpForce = 8f;
+
+    [Header("바닥 체크")]
+    [SerializeField] private float _rayLength = 1f;
+    [SerializeField] private LayerMask _groundLayer;
 
     Animator _anim;
     Rigidbody2D _rb;
@@ -22,12 +27,48 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMove();
+        JumpPlayer();
     }
 
     void PlayerMove()
     {
         float h = Input.GetAxis("Horizontal");
 
-        _anim.SetFloat("Speed", Mathf.Abs(h));
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? _playerSpeed * 2f : _playerSpeed;
+
+        _rb.linearVelocity = new Vector2(h * currentSpeed, _rb.linearVelocity.y);
+
+        _anim.SetFloat("Speed", Mathf.Abs(_rb.linearVelocity.x));
+
+        if (h > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (h < 0)
+        {
+            _spriteRenderer.flipX = true;        
+        }
+    }
+
+    void JumpPlayer()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0f);
+
+            float currentJumpForce = Input.GetKey(KeyCode.LeftShift) ? _runJumpForce : _walkJumpForce;
+
+            _rb.AddForce(Vector2.up * currentJumpForce, ForceMode2D.Impulse);
+            _anim.SetTrigger("Jump");
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _rayLength, _groundLayer);
+
+        Debug.DrawRay(transform.position, Vector3.down * _rayLength, Color.red);
+
+        return hit.collider != null;
     }
 }
