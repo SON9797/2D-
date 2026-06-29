a35f4b410f1b7f987add099731ed20c3c2ccbfb2
SON9797 <zxcvcxz95@naver.com>
2026-06-29 21:04:52 +0900

    Feat : DOTween

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,16 +1,43 @@
+using DG.Tweening;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
     int _coinScore = 1;
 
+    public Vector3 targetPos = new Vector3(9.8f, 3, 0);
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        Destroy(gameObject);
+    //
+    //        ScoreManager.instance.PlusScore(_coinScore);
+    //    }
+    //}
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Destroy(gameObject);
-
-            ScoreManager.instance.PlusScore(_coinScore);
+            StartCoroutine(DOTweenRoutine());
         }
+    }
+
+    IEnumerator DOTweenRoutine()
+    {
+        Vector3 wolrdPos = Camera.main.ViewportToScreenPoint(targetPos);
+
+        wolrdPos.z = transform.position.z;
+
+        transform.DOMove(wolrdPos, 1f);
+
+        yield return new WaitForSeconds(1f);
+
+        //Destroy(gameObject);
+        ScoreManager.instance.PlusScore(_coinScore);
     }
 }
---
+++ Assets/Scripts/CoinDOTween.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class CoinDOTween : MonoBehaviour
+{
+    private Vector3 targetPos = new Vector3(0, 5, 0);
+
+    public void DOMove()
+    {
+
+    }
+}
